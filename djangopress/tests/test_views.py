5b31e2eacec92a1b38cecf2be0a767b8c3f041d6
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-06-28 16:12:46 -0500

    Refactorizando pruebas

--- djangopress/tests/test_views.py
+++ djangopress/tests/test_views.py
@@ -4,25 +4,35 @@ from unittest.mock import Mock
 from djangopress.views import HomeView
 
 
-def test_home_view(rf, mocker):
-    """Test HomeView works"""
+def home_view_response(rf, mocker):
+    """Generate a HomeView response object."""
     option = mocker.patch('djangopress.views.Option.objects.get_or_create')
     option.return_value = (Mock(), None)
     mocker.patch('djangopress.models.Post.objects.all')
     request = rf.get('/')
     response = HomeView.as_view()(request)
+    return response
+
+
+def test_home_view(rf, mocker):
+    """Test HomeView works."""
+    response = home_view_response(rf, mocker)
     assert response.status_code == 200
 
 
-def test_home_view_context(rf, mocker):
-    """Test Homeview context"""
-    option = mocker.patch('djangopress.views.Option.objects.get_or_create')
-    option.return_value = (Mock(), None)
-    post = mocker.patch('djangopress.models.Post.objects.all')
-    request = rf.get('/')
-    response = HomeView.as_view()(request)
-    assert option.called
-    assert post.called
+def test_home_view_title_in_context(rf, mocker):
+    """Test Homeview context containts the title."""
+    response = home_view_response(rf, mocker)
     assert 'title' in response.context_data
+
+
+def test_home_view_tagline_in_context(rf, mocker):
+    """Test Homeview context contains the tagline."""
+    response = home_view_response(rf, mocker)
     assert 'tagline' in response.context_data
+
+
+def test_home_view_posts_in_context(rf, mocker):
+    """Test Homeview context contains the posts."""
+    response = home_view_response(rf, mocker)
     assert 'posts' in response.context_data
