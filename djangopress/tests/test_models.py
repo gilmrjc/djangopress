c18ef401ea759f60805d6451ffeafd2074729a50
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-06-28 16:26:13 -0500

    Refactorizando pruebas; probando get_absolute_url en Post

--- djangopress/tests/test_models.py
+++ djangopress/tests/test_models.py
@@ -1,24 +1,33 @@
 """Test for djangopress models."""
 from model_mommy import mommy
 
+from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
+
 from djangopress.models import Post, Option
 
 
 def test_post_str():
-    """Test string representation for Post object"""
+    """Test string representation for Post object."""
     post = mommy.prepare(Post)
     assert str(post) == post.title
 
 
 def test_post_slug(mocker):
-    """Test post slug is equal to post titlepost title slugified"""
+    """Test post slug is equal to post titlepost title slugified."""
     mocker.patch('django.db.models.Model.save', autospec=True)
     post = mommy.make(Post)
     assert post.slug == slugify(post.title)
 
 
+def test_post_absolute_url(mocker):
+    """Test post's absolute url."""
+    mocker.patch('django.db.models.Model.save', autospec=True)
+    post = mommy.make(Post)
+    assert post.get_absolute_url == reverse("post", kwargs={'slug': post.slug})
+
+
 def test_option_str():
-    """Test string representation for Option object"""
+    """Test string representation for Option object."""
     option = mommy.prepare(Option)
     assert str(option) == option.name
