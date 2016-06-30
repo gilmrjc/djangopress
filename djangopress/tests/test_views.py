173945430b2aa00d5a5133c3f7461fa50e8c785b
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-06-30 17:09:11 -0500

    Revert "Pruebas de paginacion"
    This reverts commit fee31e353ad49dcecdb748cb2ff0f8b60a49ccd5.

--- djangopress/tests/test_views.py
+++ djangopress/tests/test_views.py
@@ -12,14 +12,9 @@ from djangopress.models import Post
 
 def home_view_response(rf, mocker, posts=5):
     """Generate a HomeView response object."""
-    def get_or_create_util(name, **kwargs):
-        """Util function for get_or_create mock."""
-        if name == 'posts_per_page':
-            return '5', None
-        return Mock(), None
-    options = mocker.patch('djangopress.views.Option.objects.get_or_create')
-    options.side_effect = get_or_create_util
-    posts_mock = mocker.patch('djangopress.views.Post.objects.all')
+    option = mocker.patch('djangopress.views.Option.objects.get_or_create')
+    option.return_value = Mock(), None
+    posts_mock = mocker.patch('djangopress.models.Post.objects.all')
     posts_mock.return_value = mommy.prepare(Post, _quantity=posts)
     request = rf.get('/')
     response = HomeView.as_view()(request)
@@ -48,17 +43,3 @@ def test_home_view_posts_in_context(rf, mocker):
     """Test Homeview context contains the posts."""
     response = home_view_response(rf, mocker)
     assert 'posts' in response.context_data
-
-
-def test_home_no_pagination(rf, mocker):
-    """Test HomeView uses no pagination when displaying 5 posts."""
-    response = home_view_response(rf, mocker)
-    assert not response.context_data['page_obj'].has_previous()
-    assert not response.context_data['page_obj'].has_next()
-
-
-def test_home_pagination(rf, mocker):
-    """Test HomeView uses pagination when displaying 15 posts."""
-    response = home_view_response(rf, mocker, posts=15)
-    assert response.context_data['page_obj'].has_previous
-    assert response.context_data['page_obj'].has_next
