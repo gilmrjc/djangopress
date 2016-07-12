11b71cffa59cd531220afe4b4f5c3f013b7c3e8d
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-12 17:50:54 -0500

    Implementando archive_list templatetag

--- djangopress/templatetags/djangopress.py
+++ djangopress/templatetags/djangopress.py
@@ -1,9 +1,28 @@
 """Templatetags for djangopress."""
+from datetime import date
+from collections import defaultdict
+
 from django import template
 
+from djangopress.models import Post
+
+
 register = template.Library()
+
 
 @register.inclusion_tag('djangopress/tags/archive_list.html')
 def archive_list():
     """List post by date"""
-    return {}
+    posts = Post.objects.all()
+    dict = defaultdict(set)
+    for post in posts:
+        year = post.creation_date.year
+        month = post.creation_date.month
+        dict[year].add(month)
+    years = {}
+    for year, months in dict.items():
+        year = str(year)
+        years[year] = []
+        for month in months:
+            years[year].append(date( int(year), month, 1))
+    return years
