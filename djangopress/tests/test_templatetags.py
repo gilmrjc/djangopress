e15029d051bbfa12dd8c01709e94e6b731b243e1
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-12 16:41:52 -0500

    Test archive_list returns a dictionary

--- djangopress/tests/test_templatetags.py
+++ djangopress/tests/test_templatetags.py
@@ -1,8 +1,16 @@
 """Test djangopress templatetags."""
 from django.template import Template, Context
 
+from djangopress.templatetags.djangopress import archive_list
+
 
 def test_archive_list_tag():
     """Test the archive_list tag."""
     template_snippet = '{% load djangopress %}{% archive_list %}'
     Template(template_snippet).render(Context({}))
+
+
+def test_archive_list_dictionary():
+    """Test the dictionary of archive list."""
+    dictionary = archive_list()
+    assert dictionary == {}
