89a8cfa056a63f9f6280514db0d1379d7a0aa8a3
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-12 17:15:35 -0500

    Test archive_list templatetag returns a dict with dates

--- djangopress/tests/test_templatetags.py
+++ djangopress/tests/test_templatetags.py
@@ -1,7 +1,20 @@
 """Test djangopress templatetags."""
+from random import randint
+from datetime import date, timedelta
+
+from model_mommy import mommy
+
 from django.template import Template, Context
 
+from djangopress.models import Post
 from djangopress.templatetags.djangopress import archive_list
+
+
+def random_date(start, end):
+    """Generate a random date between the limits."""
+    days = int((end - start).days)
+    random_delta = timedelta(days=randint(0, days))
+    return start + random_delta
 
 
 def test_archive_list_tag():
@@ -14,3 +27,44 @@ def test_archive_list_dictionary():
     """Test the dictionary of archive list."""
     dictionary = archive_list()
     assert dictionary == {}
+
+
+def test_archive_list_posts(mocker):
+    """Test the dictionary with the months and years."""
+    posts_mock = mocker.patch('djangopress.models.Post')
+    posts = mommy.prepare(Post, _quantity=20)
+    dates = ([date(2016, 2, 1), date(2016, 2, 20)],
+             [date(2016, 2, 1), date(2016, 2, 20)],
+             [date(2016, 2, 1), date(2016, 2, 20)],
+             [date(2016, 2, 1), date(2016, 2, 20)],
+             [date(2016, 3, 1), date(2016, 3, 20)],
+             [date(2016, 3, 1), date(2016, 3, 20)],
+             [date(2016, 4, 1), date(2016, 4, 20)],
+             [date(2016, 4, 1), date(2016, 4, 20)],
+             [date(2016, 4, 1), date(2016, 4, 20)],
+             [date(2016, 4, 1), date(2016, 4, 20)],
+             [date(2016, 5, 1), date(2016, 5, 20)],
+             [date(2016, 6, 1), date(2016, 6, 20)],
+             [date(2016, 6, 1), date(2016, 6, 20)],
+             [date(2016, 7, 1), date(2016, 7, 20)],
+             [date(2016, 8, 1), date(2016, 8, 20)],
+             [date(2016, 8, 1), date(2016, 8, 20)],
+             [date(2016, 8, 1), date(2016, 8, 20)],
+             [date(2016, 9, 1), date(2016, 9, 20)],
+             [date(2016, 9, 1), date(2016, 9, 20)],
+             [date(2016, 9, 1), date(2016, 9, 20)],
+             )
+    for i, post in enumerate(posts):
+        post.creation_date = random_date(dates[i][0], dates[i][1])
+    posts_mock.return_value = posts
+    dates_dictionary = {}
+    dates_dictionary['2016'] = []
+    dates_dictionary['2016'].append(date(2016, 2, 1))
+    dates_dictionary['2016'].append(date(2016, 3, 1))
+    dates_dictionary['2016'].append(date(2016, 4, 1))
+    dates_dictionary['2016'].append(date(2016, 5, 1))
+    dates_dictionary['2016'].append(date(2016, 6, 1))
+    dates_dictionary['2016'].append(date(2016, 7, 1))
+    dates_dictionary['2016'].append(date(2016, 8, 1))
+    dates_dictionary['2016'].append(date(2016, 9, 1))
+    assert archive_list == dates_dictionary
