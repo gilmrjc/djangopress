00ac74271f983c871e949a36c3585fa60403ce7a
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-14 20:51:47 -0500

    Template for category_list (Closes #4)

--- djangopress/admin.py
+++ djangopress/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from djangopress.models import Post, Option
+from djangopress.models import Post, Option, Category
 
-# admin.site.register(UserMeta)
 admin.site.register(Option)
 admin.site.register(Post)
+admin.site.register(Category)
