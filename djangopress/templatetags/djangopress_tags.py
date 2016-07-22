1a9fb3e2b567bb7c5fa9e9f4cc1c2df7f062c5ff
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-21 21:07:13 -0500

    Added linebreakshtml template tag.
    When the text contains html is useful to ignore single
    line breaks

--- djangopress/templatetags/djangopress_tags.py
+++ djangopress/templatetags/djangopress_tags.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 
 from django import template
 from django.utils.html import escape
+from django.utils.encoding import force_text
+from django.utils.text import normalize_newlines
 from django.utils.safestring import SafeData, mark_safe
 from django.template.defaultfilters import stringfilter
 
@@ -25,6 +27,21 @@ def more(value, arg, autoescape=True):
     more_link = '<p><a href="%s" class="more-link">Read More</a><p>' % arg
     text = text[0] + more_link if len(text) > 1 else text[0]
     return mark_safe(text)
+
+
+@register.filter(is_safe=True, needs_autoscape=True)
+def linebreakshtml(value, autoescape=True):
+    """
+    Replaces double line breaks in HTML to a paragraph break (``</p>``).
+    This tag ignores single line breaks.
+    """
+    autoescape = autoescape and not isinstance(value, SafeData)
+    value = normalize_newlines(force_text(value))
+    if autoescape:
+        value = escape(value)
+    paras = re.split('\n{2,}', value)
+    paras = ['<p>%s</p>' % p for p in paras]
+    return mark_safe(''.join(paras))
 
 
 @register.inclusion_tag('djangopress/tags/archive_list.html')
