9c332090d295246068c9f80172856b3a7494f218
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2016-07-26 20:42:57 -0500

    Refactorized comments templatetags

--- djangopress/comments/templatetags/djangopress_comments.py
+++ djangopress/comments/templatetags/djangopress_comments.py
@@ -6,13 +6,13 @@ from ..models import Comment
 register = Library()
 
 
-@register.inclusion_tag('djangopress/comments/tags/comments_list.html')
+@register.inclusion_tag('comments/tags/comments_list.html')
 def show_comments(post):
     """Show the comments of the current post."""
     return {'comments': post.comment_set.all()}
 
 
-@register.inclusion_tag('djangopress/comments/tags/recent_comments.html')
+@register.inclusion_tag('comments/tags/recent_comments.html')
 def recent_comments():
     """Show the most recent comments in the blog."""
     comments = Comment.objects.all().order_by('-creation_date')[:5]
